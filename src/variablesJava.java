304432800058b4e47f23702bc6b5ae07951e0ce1
diego <diego.guerrero5dg@outlook.com>
2025-08-26 23:21:58 -0400

    Agregando descriptor de variables

---
+++ src/variablesJava.java
@@ -0,0 +1,11 @@
+public class variablesJava {
+    public static void main(String[] args) {
+        //Variables JAVA:
+        byte entero;
+        entero = 12;
+
+        byte entero2 = 12;
+        System.out.println("entero = " + entero);
+
+    }
+}
